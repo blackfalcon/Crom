21e93d23b5508d1087130b407c0ef191bf096e5d
Dale Sande <dale.sande@gmail.com>
2011-11-22 17:59:20 -0800

    making awesome happen, into the grid with you

--- crom.rb
+++ crom.rb
@@ -14,7 +14,7 @@ html_file = %w[AT.de AU.en-US BE.en-US BE.fr CA.en-US CA.fr CH.de CH.fr CH.it CO
 
 isource = %w[aut aus bel-en bel-fr can-en can-fr che-de che-fr che-it usa-en usa-es dnk fin irl nld nor nzl swe deu deu esp fra ita jpn prt]
 
-puts "\n\n Here we go ...\n\n"
+puts "\n\n \n I am an accounting program \n\n"
 
 isource.each do |isource|
   puts "isource=#{isource}"
