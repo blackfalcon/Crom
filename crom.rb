d340e4c607de44c7c6e4f3877f4bf48d76718f27
Dale Sande <dale.sande@gmail.com>
2011-11-22 19:59:05 -0800

    added some serious a.i. to make this rock

--- crom.rb
+++ crom.rb
@@ -13,24 +13,53 @@
 # isource=aut
 # /music/album/indaba-music?isource=aut_mhp_partnerCollections_Indaba
 
-
+# exmaples
 #url_prefix = "/music/album/indaba-music"
 #url_sufix = "_mhp_partnerCollections_Indaba"
 
-html_file = %w[AT.de AU.en-US BE.en-US BE.fr CA.en-US CA.fr CH.de CH.fr CH.it COM.en-US COM.es DK.en-US FI.en-US IE.en-US NL.en-US NO.en-US NZ.en-US SE.en-US de en-GB es fr ja pt-PT]
+isource = {
+	:aut => 'AT.de',
+	:aus => 'AU.en-US',
+	:bel_en => 'BE.en-US',
+	:bel_fr => 'BE.fr',
+	:can_en => 'CA.en-US',
+	:can_fr => 'CA.fr',
+	:che_de => 'CH.de',
+	:che_fr => 'CH.fr',
+	:che_it => 'CH.it',
+	:usa_en => 'COM.en-US',
+	:usa_es => 'COM.es',
+	:dnk => 'DK.en-US',
+	:fin => 'FI.en-US',
+	:irl => 'IE.en-US',
+	:nld => 'NL.en-US',
+	:nor => 'NO.en-US',
+	:nzl => 'NZ.en-US',
+	:swe => 'SE.en-US',
+	:deu => 'de',
+	:deu => 'en-GB',
+	:esp => 'es',
+	:fra => 'fr',
+	:ita => 'it',
+	:jpn => 'ja',
+	:prt => 'pt-PT' 
+	}
 
-isource = %w[?isource=aut ?isource=aus ?isource=bel-en ?isource=bel-fr ?isource=can-en ?isource=can-fr ?isource=che-de ?isource=che-fr ?isource=che-it ?isource=usa-en ?isource=usa-es ?isource=dnk ?isource=fin ?isource=irl ?isource=nld ?isource=nor ?isource=nzl ?isource=swe ?isource=deu ?isource=deu ?isource=esp ?isource=fra ?isource=ita ?isource=jpn ?isource=prt]
-
-
+# args have default values for testing and example sake	
+ARGV[0] ||= "/Footage/Frontdoor/signature"
+ARGV[1] ||= "_Home_ftv_hero_Signature"
 
 url_prefix = ARGV[0]
 url_sufix = ARGV[1]
 
 puts "\n\nI am an accounting program, I don't play games? \n\n"
 
-isource.each do |isource|
-  puts "#{isource}"
-  puts "#{url_prefix}#{isource}#{url_sufix} \n\n"
+isource.each do |key, value|
+  puts "HTML suffix=#{value}"
+  key = key.to_s.gsub '_', '-'
+  foo = "isource=#{key}"
+  puts foo
+  puts "#{url_prefix}?#{foo}#{url_sufix} \n\n"
 end
 
 puts "end of line ... \n\n"
