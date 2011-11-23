f4f568b6fe1cac0d7a532c525a8c651354ec0496
Dale Sande <dale.sande@gmail.com>
2011-11-22 18:35:48 -0800

    allows for arguments

--- crom.rb
+++ crom.rb
@@ -7,21 +7,30 @@
 #      ruby crom.rb
 #
 
-url_prefix = "/music/album/indaba-music?isource="
-url_sufix = "_mhp_partnerCollections_Indaba"
+
+# Final output is to look like the following:
+# HTML sufix=AT.de
+# isource=aut
+# /music/album/indaba-music?isource=aut_mhp_partnerCollections_Indaba
+
+
+#url_prefix = "/music/album/indaba-music?isource="
+#url_sufix = "_mhp_partnerCollections_Indaba"
 
 html_file = %w[AT.de AU.en-US BE.en-US BE.fr CA.en-US CA.fr CH.de CH.fr CH.it COM.en-US COM.es DK.en-US FI.en-US IE.en-US NL.en-US NO.en-US NZ.en-US SE.en-US de en-GB es fr ja pt-PT]
 
 isource = %w[aut aus bel-en bel-fr can-en can-fr che-de che-fr che-it usa-en usa-es dnk fin irl nld nor nzl swe deu deu esp fra ita jpn prt]
 
-puts "\n\n \n I am an accounting program \n\n"
+
+
+arg_1 = ARGV[0]
+arg_2 = ARGV[1]
+
+puts "\n\nI am an accounting program, I don't play games? \n\n"
 
 isource.each do |isource|
-  html_file.each do |html_file|
-    puts "HTML=#{html_file}"
-  end
   puts "isource=#{isource}"
-  puts "#{url_prefix}#{isource}#{url_sufix} \n\n"
+  puts "#{arg_1}#{isource}#{arg_1} \n\n"
 end
 
 puts "end of line ... \n\n"
@@ -29,7 +38,3 @@ puts "end of line ... \n\n"
 
 
 
-# Final output is to look like the following:
-# HTML sufix=AT.de
-# isource=aut
-# /music/album/indaba-music?isource=aut_mhp_partnerCollections_Indaba
